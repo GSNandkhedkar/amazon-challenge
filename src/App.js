cab636831db3cadc585705d47e46930c49dc5cb9
GSNandkhedkar <gaurinandkhedkar99@gmail.com>
2022-08-07 21:46:54 +0530

    Added react-router-dom

--- src/App.js
+++ src/App.js
@@ -2,13 +2,18 @@
 import './App.css';
 import Header from './Header';
 import Home from './Home';
+import { BrowserRouter as Router, Routes, Route} from 'react-router-dom';
 
 function App() {
   return (
-    <div className="app">
-      <Header />
-      <Home />
-    </div>
+    <Router>
+      <div className="app">
+        <Routes>
+          <Route path='/' element={[<Header/>, <Home />]}/>
+          <Route path='/checkout' element={<Header />} />
+        </Routes>
+      </div>
+    </Router>
   );
 }
 
