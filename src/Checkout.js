3971d79daf3050ff1839d37a73d1a4c7174c8a45
GSNandkhedkar <gaurinandkhedkar99@gmail.com>
2022-08-09 23:48:09 +0530

    checkout product added

--- src/Checkout.js
+++ src/Checkout.js
@@ -1,7 +1,13 @@
 import React from 'react';
 import './Checkout.css';
+import CheckoutProduct from './CheckoutProduct';
+import { useStateValue } from './StateProvider';
+import Subtotal from './Subtotal';
 
 function Checkout() {
+
+    const [{basket}, dispatch] = useStateValue();
+
   return (
     <div className='checkout'>
         <div className='checkout_left'>
@@ -13,12 +19,23 @@ function Checkout() {
 
             <div>
                 <h2 className='checkout_title'>Your shopping basket</h2>
-                {/*Basket Item */}
+                {/*CheckoutProduct */}
+                {basket.map(item => (
+                    <CheckoutProduct
+                    id={item.id}
+                    title={item.title}
+                    image={item.image}
+                    price={item.price}
+                    rating={item.rating} 
+                />
+                ))}
+                
             </div>
         </div>
 
         <div className='checkout_right'>
             <h2>Subtotal will go here</h2>
+            <Subtotal />
         </div>
     </div>
   )
--- src/Product.js
+++ src/Product.js
@@ -1,8 +1,28 @@
 import React from 'react';
 import './Product.css';
+import { useStateValue } from './StateProvider';
 
 function Product({id,title,price,rating,image}) {
-  return (
+    
+    const [{basket}, dispatch] = useStateValue();
+
+    console.log('This is the basket >>> ', basket);
+    
+    const addToBasket = () => {
+        //dispath the item into the data layer
+        dispatch({
+            type: 'ADD_TO_BASKET',
+            item: {
+                id: id,
+                title: title,
+                image: image,
+                price: price,
+                rating: rating,
+            },
+        });
+    };
+     
+    return (
     <div 
         className='product'>
         <div 
@@ -26,7 +46,7 @@ function Product({id,title,price,rating,image}) {
             alt='image' 
         />
 
-        <button>Add to Basket</button>
+        <button onClick={addToBasket}>Add to Basket</button>
 
     </div>
   )
